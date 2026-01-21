f231ac68486efa3730fe25543dd59f87556d8182
svetlogorskiy <k.svetlogorskii@automacon.ru>
2026-01-21 16:48:17 +0300

    update

--- config.tf
+++ config.tf
@@ -28,7 +28,7 @@ resource "yandex_compute_disk" "boot-disk-2" {
 }
 
 data "yandex_vpc_subnet" "network1-b" {
-  id = "e2lljsmqmc67m6ffcn92"
+  subnet_id = "e2lljsmqmc67m6ffcn92"
   # name = "default-ru-central1-b"
   # network_id = ""
   # folder_id = "b1geffa51bekseqrd9c5"
